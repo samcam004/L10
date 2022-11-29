1a9ab8bc028585f8a0d08b9aec66e2b261016a5a
Sam Campbell <samcam@umich.edu>
2022-11-29 17:36:41 -0500

    update

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -2,6 +2,8 @@
 import static org.junit.Assert.*;
 import org.junit.Assert;
 import org.junit.Test;
+import java.util.Arrays:
+
 public class testSelectionSort {
 @Test
 public void test() {
@@ -10,9 +12,12 @@ testNegative();
 testMixed();
 testDuplicates();
 }
+
+    SelectionSort sort = new SelectionSort();
+
     public testSelectionSort() {
     }
-    
+
     public void testPositive(){
         int[] arr = new int[5];
         arr[0] = 8;
@@ -20,29 +25,29 @@ testDuplicates();
         arr[2] = 7;
         arr[3] = 10;
         arr[4] = 2;
-        
+
         int[] Sortedarr = new int[5];
         Sortedarr[0] = 2;
         Sortedarr[1] = 7;
         Sortedarr[2] = 8;
         Sortedarr[3] = 9;
         Sortedarr[4] = 10;
-        
+
         /** add tests to check for this unit test **/
     }
-    
+
     public void testNegative(){
-        
+
         /** Test data contains negative values only **/
     }
-    
+
     public void testMixed(){
-        
+
         /** Test data contains with both positive, negative and zeros **/
     }
-    
+
     public void testDuplicates(){
-        
+
         /** Test data contains duplicates **/
     }
 }
