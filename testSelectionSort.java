46f2c97edc89b4e1dda83abc9fae6cb7bb0bc0bb
Sam Campbell <samcam@umich.edu>
2022-11-29 17:44:04 -0500

    Add testNegative test case

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -38,7 +38,23 @@ testPositive();
     }
 
     public void testNegative(){
+      public void testPositive(){
+          int[] arr = new int[5];
+          arr[0] = -8;
+          arr[1] = -9;
+          arr[2] = -7;
+          arr[3] = -10;
+          arr[4] = -2;
 
+          int[] Sortedarr = new int[5];
+          Sortedarr[0] = -10;
+          Sortedarr[1] = -9;
+          Sortedarr[2] = -8;
+          Sortedarr[3] = -7;
+          Sortedarr[4] = -2;
+
+          sort.basicSelectionSort(arr);
+          assert(Arrays.equals(arr,Sortedarr));
         /** Test data contains negative values only **/
     }
 
